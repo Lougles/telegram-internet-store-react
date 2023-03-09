9898201009d5bb5a7ade975dd2170407901fc4e3
vovacelidze <louglescom@gmail.com>
2023-03-09 13:09:04 +0200

    added custom telegram hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import {useEffect} from "react";
 import Header from "./components/Header/Header";
+import {useTelegram} from "./components/hooks/useTelegram";
 
 function App() {
-
+    const {tg, onToggleButton} = useTelegram();
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
   return (
     <div className="App">
-      <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
